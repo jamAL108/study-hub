09ea738ffc6e4e4d1e19e1d04257fec6c580d434
Jamal Mydeen <jamalmydeen112@gmail.com>
2024-11-22 18:52:48 +0530

    updated URL

--- webnext/src/app/home/learnmap/generate/page.tsx
+++ webnext/src/app/home/learnmap/generate/page.tsx
@@ -138,7 +138,7 @@ const Page = () => {
             })
             return;
         }
-        const URL = 'https://www.study-hub.xyz/'
+        const URL = 'https://study-hub-express.vercel.app/'
         const result: any = await fetch(`${URL}api/roadmapGeneration`, {
             method: "POST",
             headers: {
